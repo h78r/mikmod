8e888c463e3115346216eec6b3503300c214f662
h78r <rhyshrc+git@gmail.com>
2025-05-01 14:04:38 +0100

    Update builddos.sh

--- libmikmod/dos/builddos.sh
+++ libmikmod/dos/builddos.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
+cd "$(dirname "$0")"
 export PATH="/opt/djgpp/bin:$PATH"
 export CROSS=i386-pc-msdosdjgpp
 export CC=$CROSS-gcc
 export AR=$CROSS-ar
 export RANLIB=$CROSS-ranlib
-
 make -f Makefile.dj
 
 
